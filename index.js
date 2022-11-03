38617e7650cb799ce253e97032a6361233759dd5
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-11-03 06:56:41 +0600

    mongodb-added

--- index.js
+++ index.js
@@ -1,9 +1,68 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const app = express()
+require('dotenv').config()
+
 app.use(cors())
 app.use(express.json())
 const port = process.env.PORT || 5000
+app.get('/', (req, res) => {
+  res.send('genius car server is running..')
+})
 app.listen(port, () => {
   console.log(`server is rinning ${port}`)
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dnw37y6.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+
+    const servicesCollection = client.db('geniusCarDB').collection('services')
+    const ordersCollection = client.db('geniusCarDB').collection('orders')
+    app.get('/services', async(req, res) => {
+      const query = {}
+      const cursor = servicesCollection.find(query)
+      const services = await cursor.toArray()
+      res.send(services)
+     
+    })
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const service = await servicesCollection.findOne(query)
+      res.send(service)
+    })
+  // orders
+    app.get('/orders', async(req, res)=> {
+      let query = {}
+      if (req.query.email) {
+        query = {
+          email:req.query.email
+        }
+      }
+      const cursor = ordersCollection.find(query)
+      const orders = await cursor.toArray()
+      res.send(orders)
+    })
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order)
+      res.send(result)
+    })
+    app.delete('/orders/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await ordersCollection.deleteOne(query)
+      res.send(result)
+    })
+    
+  } finally {
+    
+  }
+}
+run().catch(console.dir)
